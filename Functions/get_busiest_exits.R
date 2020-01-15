0384b47e9e8efe7b262ab13d32bf5fb883e722b0
Matt Rogers <matt.rogers@rsginc.com>
2020-01-15 12:38:21 -0500

    fixed function with new unnest reqs

--- Functions/get_busiest_exits.R
+++ Functions/get_busiest_exits.R
@@ -1,12 +1,13 @@
 get_busiest_exit <- function(aggregated_toll_data){
   
   reduced_data <- aggregated_toll_data %>% 
-  group_by(weekday) %>% 
-  nest() %>% 
-  mutate(data = map(data,
-                    ~filter(., total == max(total)))) %>% 
-  unnest()
-
+    group_by(weekday) %>% 
+    nest() %>% 
+    mutate(data = map(data,
+                      ~filter(., total == max(total)))) %>% 
+    unnest(cols = c('data')) %>% 
+    ungroup()
+  
   reduced_data %<>% 
     arrange(exit_num) %>% 
     select(exit_num, exit_name, weekday, action, total)
